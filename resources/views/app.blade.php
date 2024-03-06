694d803d031305ef4f047212dfdb24f179355c2f
Nehad <35563834+81joker@users.noreply.github.com>
2024-03-06 20:49:05 +0100

    add  daisyUI themes tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,46 +1,49 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>Laravel</title>
-
-  @vite('resources/css/app.css')
-      <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-{{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.5/dist/cdn.min.js"></script> --}}
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name')}}</title>
+    @vite('resources/css/app.css')
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @livewireStyles
 
-    </head>
-    <body  class="pt-5">
+</head>
+<body data-theme="cupcake">
+    @include('layouts.navbar')
 
-<x-model name="modalOnce" title="modal1">
-{{-- @slot('body') --}}
-<x-slot:body>
-    <p class="p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto ipsam vero voluptatibus tenetur magni reprehenderit numquam ducimus cumque in! Doloremque, porro nobis. Distinctio labore nesciunt sit consequatur, perferendis delectus fugit!</p>
-{{-- @endslot --}}
-</x-slot:body>
-</x-model>
+    <x-model name="modalOnce" title="modal1">
+        {{-- @slot('body') --}}
+        <x-slot:body>
+            <p class="p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto ipsam vero
+                voluptatibus tenetur magni reprehenderit numquam ducimus cumque in! Doloremque, porro nobis. Distinctio
+                labore nesciunt sit consequatur, perferendis delectus fugit!</p>
+            {{-- @endslot --}}
+        </x-slot:body>
+    </x-model>
 
-<x-model name="modalTwo" title="Modal2">
-@slot('body')
-    <p class="p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto ipsam vero voluptatibus tenetur magni reprehenderit numquam ducimus cumque in! Doloremque, porro nobis. Distinctio labore nesciunt sit consequatur, perferendis delectus fugit!</p>
-@endslot
-</x-model>
+    <x-model name="modalTwo" title="Modal2">
+        @slot('body')
+            <p class="p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto ipsam vero voluptatibus
+                tenetur magni reprehenderit numquam ducimus cumque in! Doloremque, porro nobis. Distinctio labore nesciunt
+                sit consequatur, perferendis delectus fugit!</p>
+        @endslot
+    </x-model>
 
 
 
-<button x-data x-on:click="$dispatch('open-modal',{name: 'modalOnce'})" 
-class="px-3 py-1 bg-red-500 text-white rounded">
-Modal One
-</button>
-<button x-data x-on:click="$dispatch('open-modal',{name: 'modalTwo'})" class="px-3 py-1 bg-red-500 text-white rounded"> 
- Modal Two
-</button>
+    <button x-data x-on:click="$dispatch('open-modal',{name: 'modalOnce'})"
+        class="px-3 py-1 bg-red-500 text-white rounded ">
+        Modal One
+    </button>
+    <button x-data x-on:click="$dispatch('open-modal',{name: 'modalTwo'})"
+        class="px-3 py-1 bg-red-500 text-white rounded">
+        Modal Two
+    </button>
 
     @livewireScripts
 
-    </body>
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,10 @@ export default {
   theme: {
     extend: {},
   },
+    daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
   plugins: [],
+  plugins: [require("daisyui")],
+
 }
